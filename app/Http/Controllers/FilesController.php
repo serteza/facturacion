0d7d2fc8e055251e40f15c3b6931b705a9ced35c
franciscocamposchimal <camposfranciscoing@gmail.com>
2018-12-07 14:31:17 -0600

    fuuck

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Storage;
 
 class FilesController extends Controller
@@ -12,6 +13,7 @@ class FilesController extends Controller
         $dir = $req->rfc;
         $cerFile = $req->cer;
         $keyFile = $req->key;
+        $passphrase = $req->pass;
         $path = storage_path('app/keys/'.$dir);
         $response = " ";
 
@@ -20,6 +22,8 @@ class FilesController extends Controller
             $response = $this->checkAndStore($response, $dir, $cerFile, $keyFile);
 
             $response = $this->createPem($path, $dir, $response);
+
+            $response = $this->createPass($path, $dir, $passphrase,$response);
 
             $response = $this->createKeyPem($path, $dir, $response);
 
@@ -34,6 +38,8 @@ class FilesController extends Controller
 
             $response = $this->createPem($path, $dir, $response);
 
+            $response = $this->createPass($path, $dir, $passphrase,$response);
+            
             $response = $this->createKeyPem($path, $dir, $response);
 
             return response()->json(["message" => $response],200);
@@ -66,13 +72,35 @@ class FilesController extends Controller
         return $response;
     }
 
+    private function createPass($path, $dir, $passphrase,$response){
+
+        if(!Storage::disk('local')->exists($dir."/".$dir.".txt")){
+
+            $hashed = $this->encrypt_decrypt('encrypt', $passphrase);
+
+            $encrypted = Crypt::encryptString($hashed);
+
+            Storage::disk('local')->put($dir."/".$dir.".txt", $encrypted, 'public');
+
+            //error_log($encrypted);
+            $response .= ", el archivo .der creado ";
+
+            return $response;
+        }else{
+            $response .= ", el archivo .der ya existe ";
+        }
+
+        return $response;
+        
+    }
+
     private function createPem($path, $dir, $response){
 
         if(Storage::disk('local')->exists($dir."/".$dir.".cer")){
             $pathTocreate = $path."/".$dir.".cer";
             $cerFile =  file_get_contents($pathTocreate);
             $pemFile = "-----BEGIN CERTIFICATE-----\r\n" . chunk_split(base64_encode($cerFile), 64) . '-----END CERTIFICATE-----';
-            error_log($pemFile);
+            //error_log($pemFile);
             Storage::disk('local')->put($dir."/".$dir.".pem", $pemFile, 'public');
             $response .= ", el archivo .pem creado ";
 
@@ -84,13 +112,16 @@ class FilesController extends Controller
     private function createKeyPem($path, $dir, $response){
 
         if(Storage::disk('local')->exists($dir."/".$dir.".key")){
-        
-            $passphrase ="crh140605";
+
+            $pathTopassphrase = $path."/".$dir.".txt";
+            $passFile =  file_get_contents($pathTopassphrase);
+            $decrypted = Crypt::decryptString($passFile);
+            $hashed = $this->encrypt_decrypt('decrypt', $decrypted);
+            //error_log($hashed);
+            $passphrase = $hashed;
 
             $pathTocreate = $path."/".$dir.".key";
             $fileOut = $path."/".$dir.".key.pem";
-            //$keyFile =  file_get_contents($pathTocreate);
-            //$keypemFile = "-----BEGIN PRIVATE KEY-----\r\n" . chunk_split(base64_encode($keyFile), 64) . "-----END PRIVATE KEY-----";
             exec("openssl pkcs8 -inform DER -in ".$pathTocreate." -out ".$fileOut." -passin pass:".$passphrase);
             $response .= ", el archivo .key.pem creado ";
 
@@ -98,4 +129,24 @@ class FilesController extends Controller
         }
         
     }
+
+    function encrypt_decrypt($action, $string) {
+        $output = false;
+        $encrypt_method = "AES-256-CBC";
+        $secret_key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9';
+        $secret_iv = 'ngFIIlqydhXQV6PvXtAkLosyCSiWq4pg0OuwGvYRI';
+        // hash
+        $key = hash('sha256', $secret_key);
+        
+        // iv - encrypt method AES-256-CBC expects 16 bytes - else you will get a warning
+        $iv = substr(hash('sha256', $secret_iv), 0, 16);
+        if ( $action == 'encrypt' ) {
+            $output = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+            $output = base64_encode($output);
+        } else if( $action == 'decrypt' ) {
+            $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+        }
+        return $output;
+    }
+    
 }
